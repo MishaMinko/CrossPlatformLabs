076d1af7c1f4ec96c620c1ee3695786ae1f779f4
Misha <Misha@DESKTOP-D1T401Q>
2024-10-12 23:57:48 +0300

    added empty tests

---
+++ Lab3.Tests/Tests.cs
@@ -0,0 +1,46 @@
+namespace Lab3.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Assert.Pass();
+        }
+    }
+}
