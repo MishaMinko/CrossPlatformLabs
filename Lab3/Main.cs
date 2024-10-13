8d5b75ab7e19ba465ebb0f1b7a74fd43b4f21708
Misha <Misha@DESKTOP-D1T401Q>
2024-10-13 16:21:54 +0300

    started working with Tree

--- Lab3/Main.cs
+++ Lab3/Main.cs
@@ -1,5 +1,74 @@
-﻿namespace Lab3
+﻿using System;
+
+namespace Lab3
 {
+    public class Tree
+    {
+        public Node main;
+        public string startStr;
+
+        public Tree(string str, int index)
+        {
+            main = null!;
+            startStr = str;
+            startGenerating(index);
+        }
+
+        public void startGenerating(int index)
+        {
+            if (main == null)
+            {
+                main = new Node(index);
+                main.doOperation(startStr);
+            }
+
+            if(main.checkIfFinished())
+            {
+                deepDive(main.left, 1, main.strNow);
+                deepDive(main.right, 1, main.strNow);
+            }
+        }
+
+        public void deepDive(Node node, int currentDive, string currentStr)
+        {
+
+        }
+    }
+    public class Node
+    {
+        public int index;
+        public string stepDone;
+        public string strNow;
+        public Node left;
+        public Node right;
+
+        public Node(int index)
+        {
+            this.index = index;
+            stepDone = "0";
+            strNow = "0";
+            left = null!;
+            right = null!;
+        }
+
+        public void doOperation(string str)
+        {
+            strNow = str;
+
+            //insertBall(ball);
+        }
+
+        public void insertBall(string ball)
+        {
+            strNow.Insert(index, ball);
+        }
+
+        public bool checkIfFinished()
+        {
+            return strNow.Equals("");
+        }
+    }
+
     public class Main
     {
         public string[] getDataFromFile(string path)
@@ -20,6 +89,15 @@
             return false;
         }
 
+        public Tree[] generateSteps(string str)
+        {
+            Tree[] res = new Tree[str.Length + 1];
+
+            for (int i = 0; i < str.Length + 1; i++)
+                res[i] = new Tree(str, i);
+
+            return res;
+        }
 
         public void Start()
         {
@@ -52,8 +130,9 @@
             }
 
             Console.WriteLine("Entered balls: " + balls);
+            Console.WriteLine(balls.Insert(balls.Length, "W"));
 
-            //long[] sequence = generateSequence(numberOfSteps);
+            //Tree[] results = generateSteps(balls);
 
             //Console.WriteLine("Sequence:");
             //for (int i = 0; i < sequence.Length; i++)
