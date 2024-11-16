f0ca664312482a16677881ef057d667618f3d166
Minko <mf0981115811@gmail.com>
2024-11-16 13:08:48 +0200

    program start mcmaster

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using McMaster.Extensions.CommandLineUtils;
+
+
+[Command(Name = "Lab4", Description = "Open labs app")]
+class Program
+{
+    static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+    private void OnExecute()
+    {
+        Console.WriteLine("Enter command");
+    }
+
+    private void OnUnknownCommand(CommandLineApplication app)
+    {
+        Console.WriteLine("Unknown command. Use one of the following:");
+        Console.WriteLine(" - version: Displays app version and author");
+        Console.WriteLine(" - run: Run a specific lab");
+        Console.WriteLine(" - set-path: Set input/output path");
+    }
+}
